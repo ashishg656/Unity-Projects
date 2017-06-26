fd6ac0c331a52a34312430b86ba5bc89bd61656a
Ashish Goel <ashishg656@gmail.com>
2017-06-27 01:02:42 +0530

    catch it game tutorial and flappy bird CP

---
+++ Animation tutorial/Assets/Hat Trick - The Catch Game/Scripts/HatController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatController : MonoBehaviour {
+
+	public Camera cam;
+	Rigidbody2D rb2d;
+	Renderer render;
+
+	private float maxWidth;
+
+	// Use this for initialization
+	void Start () {
+		if (cam == null) {
+			cam = Camera.main;
+		}
+		rb2d = GetComponent<Rigidbody2D> ();
+		render = GetComponent<Renderer> ();
+
+		Vector3 upperCorner = new Vector3 (Screen.width, Screen.height, 0f);
+		Vector3 targetWidth = cam.ScreenToWorldPoint (upperCorner);
+		// calculating the width of the hat object dynamically
+		float hatWidth = render.bounds.extents.x;
+		maxWidth = targetWidth.x - hatWidth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// called once per physics timestamp
+	void FixedUpdate(){
+		Vector3 rawPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+		Vector3 targetPosition = new Vector3 (rawPosition.x, 0f, 0f);
+
+		targetPosition.x = Mathf.Clamp (targetPosition.x, -maxWidth, maxWidth);
+		rb2d.MovePosition (targetPosition);
+	}
+}
---
+++ Flappy CP/Assets/Scripts/bird.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+	public float upForce = 200f;
+	Animator anim;
+	public bool isDead = false;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonDown (0) && !isDead) {
+//			rb2d.velocity = //new Vector2 (0, 0); 
+			rb2d.velocity = Vector2.zero;
+			rb2d.AddForce (new Vector2 (0, upForce));
+
+			anim.SetTrigger ("Flap");
+		}
+	}
+
+	public void OnCollisionEnter2D(Collision2D other){
+		anim.SetTrigger ("Die");
+		isDead = true;
+	}
+}
